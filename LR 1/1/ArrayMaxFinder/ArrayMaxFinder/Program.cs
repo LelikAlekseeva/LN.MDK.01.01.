e4edc04e53ef69ba520463e4d828c3c0d707d4f9
leliK <leliK@LELIK>
2025-11-10 00:37:09 +0300

    создали массив, где пользователь задаёт размер и заполяет его эллементами

---
+++ LR 1/1/ArrayMaxFinder/ArrayMaxFinder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ArrayMaxFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество жлементов массива: ");
+            int n =int.Parse(Console.ReadLine()); ///спрашиваем размер массива у пользователя 
+            int[] array = new int[n];///создание массива заданного размера 
+
+            Console.WriteLine($"Введите {n} целых чисел : ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Элемент {i + 1}: ");
+                array[i] = int.Parse(Console.ReadLine());///заполняется массив чиселками пользователя
+            }
+        }
+    }
+}
