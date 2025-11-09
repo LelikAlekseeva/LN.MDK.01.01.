fd57dc186c1074d85c84b782dac9a4b39cacd5da
leliK <leliK@LELIK>
2025-11-09 23:56:06 +0300

    Добавьте файлы проекта.

---
+++ ArrayMaxFinder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMaxFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов массива: ");
+            int n =int.Parse(Console.ReadLine()); ///спрашиваем у пользоватля размер массива
+        }
+    }
+}
