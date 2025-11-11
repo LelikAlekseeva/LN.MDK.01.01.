9f3d1fe3a1f64ff28707eaf66efbecd5d66436de
leliK <leliK@LELIK>
2025-11-11 04:13:12 +0300

    создали список для хранения чисел и заполнили 1500 числами от1 до 201

---
+++ Lr1/lab 2/Array/UniqueNumbersFinder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UniqueNumbersFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();///Создаем генератор случайных чисел
+            List<int> allNumbers = new List<int>();///Создаем список для хранения 1500 случайных чисел 
+
+            Console.WriteLine("Генерируем 1500 случайных чисел...");///Заполняем список 1500 случайными числами от 1 до 200
+            for (int i = 0; i < 1500; i++)
+            {
+                int randomNumber = random.Next(1, 201); // Генерируем число от 1 до 200
+                allNumbers.Add(randomNumber); // Добавляем число в список
+            }
+    }
+}
