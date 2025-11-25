8f0d05c2616b4962ce54c42536d6281cef71b6e9
KateSmirnova1 <20katerina0060@gmail.com>
2025-11-25 13:23:29 +0300

    Сделать вывод разобр

---
+++ ManyClasses/ManyClasses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Products mersedes = new Products();
+            mersedes.SetName("Мерседес");
+            mersedes.SetPrice(1000000);
+            Products uaz = new Products();
+            uaz.SetName("Уаз");
+            uaz.SetPrice(10000000);
+            Storage storage = new Storage();
+            storage.SetId(123);
+            storage.SetLocation("Торжок, улица Студенская 3");
+            storage.SetProductQuantity(mersedes, 2);
+            storage.SetProductQuantity(uaz, 5);
+
+        }
+    }
+}
---
+++ ManyClasses/ManyClasses/Storage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyClasses
+{
+    public class Storage
+    {
+        private int id_;
+        private string location_;
+        private Dictionary<Products, int> products_;
+        public void SetId(int id)
+        {
+            id_ = id;
+        }
+
+        public void SetLocation (string location)
+        {
+            location_ = location;
+        }
+        public void SetProductQuantity(Products product, int quantity)
+        {
+            products_.Add(product, quantity);
+        }
+
+    }
+}
