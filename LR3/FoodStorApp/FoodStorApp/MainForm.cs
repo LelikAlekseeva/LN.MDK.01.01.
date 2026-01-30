7206de6ecd49f4af35abe08551f258a515825e17
Fa1tloy <baranovdaniil534@gmail.com>
2026-01-30 11:25:53 +0300

    в классе

---
+++ LR3/FoodStorApp/FoodStorApp/MainForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodStorApp
+{
+    public partial class MainForm : Form
+    {
+        private Dictionary<string, List<Product>> productGroups = new Dictionary<string, List<Product>>();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LR3/FoodStorApp/FoodStorApp/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodStorApp
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Supplier { get; set; }
+        public string Manufacture{ get; set; }
+        public string ExpiryDate{ get; set; }
+        public string Description { get; set; }
+        public Product(string name, decimal price, string supplier, string manufacture, string expiryDate, string description)
+        {
+            Name = name;
+            Price = price;
+            Supplier = supplier;
+            Manufacture = manufacture;
+            ExpiryDate = expiryDate;
+            Description = $"Цена: {price} руб.\nПоставщик: {supplier}\nПроизводитель: {manufacture}\nСрок годности: {expiryDate}";
+        }
+        public override string ToSrting()
+        {
+            return $"{Name} - {price}руб.";
+        }
+    }
+}
