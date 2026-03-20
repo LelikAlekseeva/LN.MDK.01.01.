5d2f2851407692c5a8979a6203f1def25eaae752
= <=>
2026-03-20 09:38:56 +0300

    Работа на паре

---
+++ TestBd/TestBd/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace TestBd
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usersDataGridView = new System.Windows.Forms.DataGridView();
+            this.DeleteUser = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.usersDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // usersDataGridView
+            // 
+            this.usersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.usersDataGridView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.usersDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.usersDataGridView.Name = "usersDataGridView";
+            this.usersDataGridView.Size = new System.Drawing.Size(800, 150);
+            this.usersDataGridView.TabIndex = 0;
+            // 
+            // DeleteUser
+            // 
+            this.DeleteUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DeleteUser.Location = new System.Drawing.Point(26, 371);
+            this.DeleteUser.Name = "DeleteUser";
+            this.DeleteUser.Size = new System.Drawing.Size(99, 52);
+            this.DeleteUser.TabIndex = 1;
+            this.DeleteUser.Text = "Удалить ";
+            this.DeleteUser.UseVisualStyleBackColor = true;
+            this.DeleteUser.Click += new System.EventHandler(this.DeleteUser_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DeleteUser);
+            this.Controls.Add(this.usersDataGridView);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.usersDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView usersDataGridView;
+        private System.Windows.Forms.Button DeleteUser;
+    }
+}
+
